b652533d3e6b7f73cce780d3843c949e8805a53a
EGOIST <0x142857@gmail.com>
2022-02-20 16:39:23 +0800

    Tweak umami URL

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
             async
             defer
             data-website-id="4ff6897f-d635-4c16-8c67-91c40fae7281"
-            src="https://umami.egoist.sh/umami.js"
+            src="https://umami.egoist.sh/mami.js"
           ></script>
         )}
       </body>
