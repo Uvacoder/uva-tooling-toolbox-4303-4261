21e5fcdca7889eb2be02e516581d9b37441ca3d6
EGOIST <0x142857@gmail.com>
2022-02-20 15:39:40 +0800

    Add link to GitHub

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,29 @@ import { tools } from "../utils/tools"
 export default function HomePage() {
   return (
     <Layout>
-      <div className="grid grid-cols-3 gap-5 p-5">
+      <div className="p-5 max-w-screen-lg">
+        <div className="bg-cyan-100 text-cyan-700 font-bold p-5 rounded-lg space-y-3">
+          <p>
+            tooling.one is a collection of useful tools for developers, it works
+            locally mostly, no data will be uploaded to any server unless
+            otherwise noted.
+          </p>
+          <p className="">
+            I'm keeping adding new tools to this website, you can also suggest
+            ideas on{" "}
+            <a
+              href="https://github.com/egoist/tooling.one/issues"
+              className="underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-3 gap-5 p-5 max-w-screen-lg">
         {tools.map((tool) => {
           return (
             <Link href={tool.link} key={tool.name}>
