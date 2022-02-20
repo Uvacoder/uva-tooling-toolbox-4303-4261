3f7aac33205a1cad867e0502c58f1ec4a2745c3b
EGOIST <0x142857@gmail.com>
2022-02-20 17:55:13 +0800

    Fis ssr issue

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ module.exports = {
     // Needed by `htmltojsx` package
     config.plugins.push(
       new webpack.DefinePlugin({
-        IN_BROWSER: !isServer,
+        IN_BROWSER: true,
       })
     )
 
     // Replace React with Preact in client production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
+        react$: "preact/compat",
+        "react-dom/test-utils$": "preact/test-utils",
+        "react-dom$": "preact/compat",
       })
     }
 
