482d435d409f49f7d6d16669e3bd62ff7b1bfeec
EGOIST <0x142857@gmail.com>
2022-02-21 21:53:54 +0800

    Import diff editor UI

--- src/pages/text-diff.tsx
+++ src/pages/text-diff.tsx
@@ -1,8 +1,6 @@
 import type { editor as MonacoEditor } from "monaco-editor"
 import { useEffect, useRef, useState } from "react"
 import { Layout } from "../components/Layout"
-import { Textarea } from "../components/Textarea"
-import { Column, TwoColumns } from "../components/TwoColumns"
 import { MONACO_LANGUAGES, MONACO_PATH } from "../generated/monaco-editor"
 
 const loadMonaco = async (): Promise<typeof import("monaco-editor")> => {
@@ -31,22 +29,55 @@ const getMonaco = (): typeof import("monaco-editor") | undefined => {
   return window.monaco
 }
 
+const getMonacoFileUrl = (language: string, filename: string) => {
+  const monaco = getMonaco()
+  const isTS = language === "typescript"
+  const isJS = language === "javascript"
+  if (isTS || isJS) {
+    return monaco.Uri.file(filename + (isTS ? ".tsx" : ".jsx"))
+  }
+  return undefined
+}
+
 export default function TextDiffPage() {
-  const [original, setOriginal] = useState("Hello World")
-  const [modified, setModified] = useState("Goodbye World")
   const editorContainer = useRef<HTMLDivElement | null>(null)
   const [language, setLanguage] = useState("text")
+  const [inlineView, setInlineView] = useState(false)
 
   const [diffEditor, setDiffEditor] =
     useState<MonacoEditor.IStandaloneDiffEditor | null>(null)
 
+  const createModel = (
+    value: string,
+    language: string,
+    type: "original" | "modified"
+  ) => {
+    return monaco.editor.createModel(
+      value,
+      language,
+      getMonacoFileUrl(language, type)
+    )
+  }
+
   const initEditor = async () => {
     const monaco = await loadMonaco()
-    const originalModel = monaco.editor.createModel(original, language)
-    const modifiedModel = monaco.editor.createModel(modified, language)
+    // Disable type errors
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: false,
+    })
+
+    // Tell typescript to use 'react' for jsx files.
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      jsx: monaco.languages.typescript.JsxEmit.React,
+    })
+
+    const originalModel = createModel(`Hello World`, language, "original")
+    const modifiedModel = createModel(`Goodbye World`, language, "modified")
     const editor = monaco.editor.createDiffEditor(editorContainer.current, {
       enableSplitViewResizing: false,
-      renderSideBySide: false,
+      renderSideBySide: !inlineView,
+      originalEditable: true,
     })
     editor.setModel({
       original: originalModel,
@@ -66,23 +97,23 @@ export default function TextDiffPage() {
 
   useEffect(() => {
     if (diffEditor) {
-      diffEditor.getOriginalEditor().setValue(original)
-    }
-  }, [original])
-
-  useEffect(() => {
-    if (diffEditor) {
-      diffEditor.getModifiedEditor().setValue(modified)
-    }
-  }, [modified])
-
-  useEffect(() => {
-    if (diffEditor) {
       const monaco = getMonaco()
       if (monaco) {
         const models = diffEditor.getModel()
-        if (models) {
-          console.log(models.modified.getLanguageId(), language)
+
+        if (language === "typescript" || language === "javascript") {
+          // Currently no way to change model uri, so we have to recreate the model
+          const orignal = models.original.getValue()
+          const modified = models.modified.getValue()
+          models.original.dispose()
+          models.modified.dispose()
+          const originalModel = createModel(orignal, language, "original")
+          const modifiedModel = createModel(modified, language, "modified")
+          diffEditor.setModel({
+            original: originalModel,
+            modified: modifiedModel,
+          })
+        } else {
           monaco.editor.setModelLanguage(models.modified, language)
           monaco.editor.setModelLanguage(models.original, language)
         }
@@ -90,39 +121,42 @@ export default function TextDiffPage() {
     }
   }, [language])
 
+  useEffect(() => {
+    if (diffEditor) {
+      diffEditor.updateOptions({
+        renderSideBySide: !inlineView,
+      })
+    }
+  }, [inlineView])
+
   return (
     <Layout>
-      <TwoColumns>
-        <Column title="Text">
-          <div className="mb-5">
-            <label className="text-sm text-zinc-400 mr-1">
-              Choose a lanuage:
-            </label>
-            <select
-              value={language}
-              onChange={(e) => setLanguage(e.target.value)}
-            >
-              {MONACO_LANGUAGES.map((lang) => {
-                return (
-                  <option key={lang} value={lang}>
-                    {lang}
-                  </option>
-                )
-              })}
-            </select>
-          </div>
-          <div className="space-y-5">
-            <Textarea fullWidth value={original} onChange={setOriginal} />
-            <Textarea fullWidth value={modified} onChange={setModified} />
-          </div>
-        </Column>
-        <Column title="Result">
-          <div
-            ref={editorContainer}
-            className="h-[600px] border rounded-lg overflow-hidden"
-          ></div>
-        </Column>
-      </TwoColumns>
+      <div className="h-screen flex flex-col">
+        <header className="h-12 border-b flex-shrink-0 flex items-center px-3 space-x-5">
+          <select
+            value={language}
+            className="bg-white p-1 border rounded-lg"
+            onChange={(e) => setLanguage(e.target.value)}
+          >
+            {MONACO_LANGUAGES.map((lang) => {
+              return (
+                <option key={lang} value={lang}>
+                  {lang}
+                </option>
+              )
+            })}
+          </select>
+          <label className="space-x-1 flex items-center">
+            <input
+              type="checkbox"
+              checked={inlineView}
+              onChange={(e) => setInlineView(e.target.checked)}
+            />
+            <span>Inline View</span>
+          </label>
+        </header>
+        <div ref={editorContainer} className="h-full"></div>
+      </div>
     </Layout>
   )
 }
