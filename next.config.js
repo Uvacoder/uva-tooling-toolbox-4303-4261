57965dd593ff3bc801c768a4f6ba93cc8ef03ccd
EGOIST <0x142857@gmail.com>
2022-08-15 23:31:31 +0800

    update deps

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
     PKG_VERSION: require("./package.json").version,
   },
   webpack(config, { dev, isServer }) {
-    // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      })
-    }
-
     config.experiments = config.experiments || {}
     Object.assign(config.experiments, {
       asyncWebAssembly: true,
