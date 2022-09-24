f1dc4c5e1a6811e0c2bff92b7037cec838126997
Uvacoder <uva22940coder@gmail.com>
2022-09-24 03:11:33 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,20 @@ export default function HomePage() {
           />
           <span>Welcome to the Toolbox.</span>
         </a>
-        <div className="bg-cyan-100 text-cyan-700 font-bold p-5 rounded-lg space-y-3">
+        
+      </div>
+      <div className="grid md:grid-cols-3 gap-5 p-5 max-w-screen-lg">
+        {tools.map((tool) => {
+          return (
+            <Link href={tool.link} key={tool.name}>
+              <a className="border rounded-lg p-5 font-bold hover:bg-zinc-100">
+                {tool.name}
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+      <div className="bg-cyan-100 text-cyan-700 font-bold p-5 rounded-lg space-y-3">
           <p>
             The Toolbox is a collection of useful tools for developers, it works
             locally mostly, no data will be uploaded to any server unless
@@ -38,18 +51,6 @@ export default function HomePage() {
             .
           </p>
         </div>
-      </div>
-      <div className="grid md:grid-cols-3 gap-5 p-5 max-w-screen-lg">
-        {tools.map((tool) => {
-          return (
-            <Link href={tool.link} key={tool.name}>
-              <a className="border rounded-lg p-5 font-bold hover:bg-zinc-100">
-                {tool.name}
-              </a>
-            </Link>
-          )
-        })}
-      </div>
     </Layout>
   )
 }
